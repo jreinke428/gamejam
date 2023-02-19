fd620e5cdb5be64975668a135f6cba3587e1e5b7
naroop <nrupp815@outlook.com>
2023-02-18 21:12:29 -0600

    enemy damage

--- Player/Gun/Bullet/Bullet.gd
+++ Player/Gun/Bullet/Bullet.gd
@@ -1,11 +1,21 @@
 extends Area2D
 
-const RIGHT = Vector2.RIGHT
-@export var SPEED: int = 200
+var speed: int = 200
+var damage: int = 1
 
 func _physics_process(delta):
-	var movement = RIGHT.rotated(rotation) * SPEED * delta
+	var movement = Vector2.RIGHT.rotated(rotation) * speed * delta
 	global_position += movement
+	
+func initializeBullet(speed: int, damage: int):
+	speed = speed
+	damage = damage
 	
 func _on_visible_on_screen_notifier_2d_screen_exited():
 	queue_free()
+
+
+func _on_area_entered(area):
+	if area.get_parent().is_in_group("Enemy"):
+		area.get_parent().hit(damage)
+		queue_free()
--- Player/Gun/Gun.gd
+++ Player/Gun/Gun.gd
@@ -3,6 +3,9 @@ extends Sprite2D
 @export var bullet: PackedScene = null
 @onready var fireRateTimer = $FireRate
 
+var bulletSpeed := 205
+var bulletDamage := 2
+
 func _ready():
 	fireRateTimer.wait_time = 0.3
 
@@ -13,6 +16,8 @@ func _process(delta):
 func tryShootGun():
 	if fireRateTimer.is_stopped():
 		var newBullet: Area2D = bullet.instantiate()
+		newBullet.initializeBullet(bulletSpeed, bulletDamage)
+		
 		get_tree().current_scene.add_child(newBullet)
 		newBullet.global_position = global_position + global_position.direction_to(get_global_mouse_position()) * 11
 		newBullet.global_rotation = global_rotation
